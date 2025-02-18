9e4519b94f64416f2fe5f831989e3c19dcfd9f7d
snOlga <olga.safonova@outlook.com>
2025-02-19 00:01:37 +0300

    moving location with character

--- js/Going.js
+++ js/Going.js
@@ -1,53 +1,64 @@
-const SPEED = 1
+const SPEED = 2
+const MIN_X = 0
+const MAX_X = 3200
+const RIGHT_DIRECTION = true
+const LEFT_DIRECTION = false
+const CHARACTER_ID = 'character'
+const LOCATION_ID = 'location'
 
 document.getElementById('field').addEventListener('click', (e) => resetCharacterX(e))
 
-const MIN_X = 0
-const MAX_X = 6400
-
 async function resetCharacterX(event) {
     const newX = event.clientX
-    const oldX = getLeftComputedStyle('character')
+    const oldX = getLeftComputedStyle(CHARACTER_ID)
 
     if (newX > oldX)
-        wrapMoveAnimation('character-go-right', () => moveRight(newX))
+        wrapMoveAnimation('character-go-right', () => move(newX, RIGHT_DIRECTION))
     else
-        wrapMoveAnimation('character-go-left', () => moveLeft(newX))
+        wrapMoveAnimation('character-go-left', () => move(newX, LEFT_DIRECTION))
 }
 
 function wrapMoveAnimation(animation, movingFunc) {
-    document.getElementById('character').classList.add(animation)
+    document.getElementById(CHARACTER_ID).classList.add(animation)
     movingFunc()
-    document.getElementById('character').classList.remove(animation)
+    document.getElementById(CHARACTER_ID).classList.remove(animation)
 }
 
-async function moveRight(newX) {
-    const oldX = getCharacterXonLocation()
-    const newLocationX = getLeftComputedStyle('location') + newX
-    for (let movement = oldX; movement < newLocationX; movement++) {
-        let characterPosition = getLeftComputedStyle('character')
-        if (!moveLocationRight(characterPosition)) 
-            setLeftStyle('character', characterPosition + 1)
+async function move(newX, isRightDirection) {
+    const oldX = getLeftComputedStyle(LOCATION_ID) + getLeftComputedStyle(CHARACTER_ID)
+    const newLocationX = getLeftComputedStyle(LOCATION_ID) + newX
+
+    for (let movement = oldX;
+        isRightDirection && movement <= newLocationX ||
+        !isRightDirection && movement > newLocationX;
+        movement = movement + 1 * (isRightDirection ? 1 : - 1)
+    ) {
+        let characterPosition = getLeftComputedStyle(CHARACTER_ID)
+        let leftLocationPosition = getLeftComputedStyle(LOCATION_ID)
+
+        if (!shouldMoveLocation(characterPosition))
+            setLeftStyle(CHARACTER_ID, characterPosition + 1 * (isRightDirection ? 1 : -1))
+        else
+            setLeftStyle(LOCATION_ID, leftLocationPosition - 1 * (isRightDirection ? 1 : -1))
+
+        preventDeadZoneLocation()
         await sleep(SPEED)
     }
 }
 
-async function moveLeft(newX) {
-    for (let movement = oldX; movement > newX; movement--) {
-        setLeftStyle('character', movement)
-        await sleep(SPEED)
-    }
-}
-
-function moveLocationRight(newCharacterX) {
-    const rightPosition = getRightComputedStyle('location')
+function shouldMoveLocation(newCharacterX) {
+    const leftPosition = getLeftComputedStyle(LOCATION_ID)
+    const rightPosition = getRightComputedStyle(LOCATION_ID)
     const isCharacterOnCenter = newCharacterX == (document.body.clientWidth / 2)
-    console.log(rightPosition)
-    if (rightPosition < 0 && isCharacterOnCenter)
-        setRightStyle('location', rightPosition + 1) // TODO: stopped here
-    return rightPosition < 0 && isCharacterOnCenter
+    return leftPosition < 0 && rightPosition < 0 && isCharacterOnCenter
 }
 
-function getCharacterXonLocation() {
-    return getLeftComputedStyle('location') + getLeftComputedStyle('character')
+function preventDeadZoneLocation() {
+    const leftPosition = getLeftComputedStyle(LOCATION_ID)
+    const rightPosition = getRightComputedStyle(LOCATION_ID)
+    let leftLocationPosition = getLeftComputedStyle(LOCATION_ID)
+    if (rightPosition == 0)
+        setLeftStyle(LOCATION_ID, leftLocationPosition + 1)
+    if (leftPosition == 0)
+        setLeftStyle(LOCATION_ID, leftLocationPosition - 1)
 }
